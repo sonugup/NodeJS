c15286c0024d0152dca5c520931da0bdae37ce07
Sonu Gupta <rg3029926@gmail.com>
2023-08-30 16:20:35 +0530

    add node

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,21 +1,22 @@
-const http=require("http")
-const fs=require("fs")
+const http = require("http");
+const fs = require("fs");
 
+const sever = http.createServer((req, res) => {
+  if (req.url == "/") {
+    fs.writeFile("tesk.txt", "welcome to node js");
+    // fs.appendFile("tesk.txt", "Hi I am Sonu Gupta")
+    res.setHeader("Content-Type", "text/html");
+    res.end(` <h1>welcome</h1>`);
+  } else if (req.url == "/test") {
+    const readf = fs.readFile("tesk.txt", "utf-8");
+    console.log(readf);
 
-const sever=http.createServer( (req,res) => {
-    if(req.url=="/"){
-        fs.readFileSync("/test.txt", "welcome to node js")
-        res.end("welcome")
-    }
-    else if(req.url=="/test"){
-        res.end("welcome to test page")
-    }
-     else{
-        res.end("try again")
-    }
-})
-
+    res.end("welcome to test page");
+  } else {
+    res.end("try again");
+  }
+});
 
 sever.listen(2300, () => {
-    console.log("happy")
-})
+  console.log("happy");
+});
